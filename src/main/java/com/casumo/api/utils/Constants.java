eed978940299a304261a95b58093226f470e658d
alsofuoglu <Abdullatif.Sofuoglu@seavus.com>
2022-11-22 17:24:55 +0300

    Small fixes

--- src/main/java/com/casumo/api/utils/Constants.java
+++ src/main/java/com/casumo/api/utils/Constants.java
@@ -1,9 +1,6 @@
 package com.casumo.api.utils;
 
 public class Constants {
-    public static final String TEST_CASE_DIR = "/testData/";
-    public static final String JSON_EXT = ".json";
-    public static final String STATUS_CODE = "StatusCode";
     public static final String PASSED = "PASSED";
     public static final String SHEET_NAME = "input";
     public static final String TEST_DATA_XSLX_PATH = "\"src/test/resources/TestData.xlsx\"";
