4f8f9e721b2288a081efe84fe6e09630a1a21d6d
alsofuoglu <Abdullatif.Sofuoglu@seavus.com>
2022-11-22 17:13:57 +0300

    Updated constans

--- src/main/java/com/casumo/api/utils/Constants.java
+++ src/main/java/com/casumo/api/utils/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
     public static final String TEST_CASE_DIR = "/testData/";
     public static final String JSON_EXT = ".json";
     public static final String STATUS_CODE = "StatusCode";
+    public static final String PASSED = "PASSED";
+    public static final String SHEET_NAME = "input";
+    public static final String TEST_DATA_XSLX_PATH = "\"src/test/resources/TestData.xlsx\"";
+
 }
--- src/test/java/test/VowelTest.java
+++ src/test/java/test/VowelTest.java
@@ -1,7 +1,9 @@
 package test;
 
 import static io.restassured.RestAssured.given;
+
 import com.casumo.api.utils.ApiHelper;
+import com.casumo.api.utils.Constants;
 import com.casumo.api.utils.ExcelUtil;
 import com.casumo.api.utils.ResponseConstants;
 import io.restassured.RestAssured;
@@ -11,40 +13,41 @@ import org.testng.annotations.Test;
 
 public class VowelTest extends TestBase {
 
-  @Test(dataProvider = "testData")
-  public void vowelTest(String input) {
+    @Test(dataProvider = "testData")
+    public void vowelTest(String input) {
 
-    extentLogger = report.createTest("Vowel Test");
-    RestAssured.baseURI = props.getProperty("api.url");
+        extentLogger = report.createTest("Vowel Test");
+        RestAssured.baseURI = props.getProperty("api.url");
 
-    extentLogger.info("Get input from xlsx file");
-    extentLogger.info("Input is : " + input);
-    String actualResponse =
-        given()
-            .when()
-            .get(props.getProperty("api.path") + input)
-            .then()
-            .assertThat()
-            .statusCode(ResponseConstants.STATUS_CODE_OK)
-            .and()
-            .extract()
-            .response()
-            .asString();
+        extentLogger.info("Get input from xlsx file");
+        extentLogger.info("Input is : " + input);
+        String actualResponse =
+                given()
+                        .when()
+                        .get(props.getProperty("api.path") + input)
+                        .then()
+                        .assertThat()
+                        .statusCode(ResponseConstants.STATUS_CODE_OK)
+                        .and()
+                        .extract()
+                        .response()
+                        .asString();
 
-    extentLogger.info("Actual response   is : " + actualResponse);
-    System.out.println("Response is :" + actualResponse);
+        extentLogger.info("Actual response   is : " + actualResponse);
+        System.out.println("Response is :" + actualResponse);
 
-    String expectedResponse = ":" + ApiHelper.extractVowels(input);
-    extentLogger.info("Expected response is : " + actualResponse);
+        String expectedResponse = ":" + ApiHelper.extractVowelsRecursive(input).trim();
+        actualResponse = actualResponse.trim();
+        extentLogger.info("Expected response is : " + actualResponse);
 
-    Assert.assertEquals(actualResponse, expectedResponse, "Expected response is not correct.");
-    extentLogger.pass("PASSED");
-  }
+        Assert.assertEquals(actualResponse, expectedResponse, "Expected response is not correct.");
+        extentLogger.pass(Constants.PASSED);
+    }
 
-  @DataProvider
-  public Object[][] testData() {
-    ExcelUtil qa3short = new ExcelUtil("src/test/resources/TestData.xlsx", "input");
-    String[][] dataArray = qa3short.getDataArrayWithoutFirstRow();
-    return dataArray;
-  }
+    @DataProvider
+    public Object[][] testData() {
+        ExcelUtil qa3short = new ExcelUtil(Constants.TEST_DATA_XSLX_PATH, Constants.SHEET_NAME);
+        String[][] dataArray = qa3short.getDataArrayWithoutFirstRow();
+        return dataArray;
+    }
 }
