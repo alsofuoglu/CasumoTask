c0fb13d3de65b7783543b6474a8bb3855b5b7b34
alsofuoglu <Abdullatif.Sofuoglu@seavus.com>
2022-11-23 12:02:24 +0300

    Updated test data file

--- src/main/java/com/casumo/api/utils/Constants.java
+++ src/main/java/com/casumo/api/utils/Constants.java
@@ -3,6 +3,6 @@ package com.casumo.api.utils;
 public class Constants {
     public static final String PASSED = "PASSED";
     public static final String SHEET_NAME = "input";
-    public static final String TEST_DATA_XSLX_PATH = "\"src/test/resources/TestData.xlsx\"";
+    public static final String TEST_DATA_XSLX_PATH = "src/test/resources/TestData.xlsx";
 
 }
